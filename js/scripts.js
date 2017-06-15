e686efb91faff4e3a7ede3c3ea483ea497889c9f
Kirsten Knapp <kknapp@alumni.berklee.edu>
2017-06-15 15:05:50 -0700

    Add changes to html, css to make it a shopping website. Add jQuery and javaScript - not working yet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    var blanks = ["person1", "person2", "animal", "petName", "exclamation1", "exclamation2", "verb1", "verb2", "adjective1", "adjective2", "adjective3", "adjective4"];
+  $(".list").hide();
+  $("#button").on("click",function() {
+    var blanks = ["item1", "item2", "item3", "item4"];
 
-    blanks.forEach(function(blank) {
+    blanks.sort();
+
+    var capBlanks = blanks.map(function(blank) {
+      return blank.toUpperCase();
+    });
+
+    capBlanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput);
     });
 
-    $("#story").show();
+
+    $(".story").show();
 
     event.preventDefault();
   });
 });
-
-
-
-
-    
